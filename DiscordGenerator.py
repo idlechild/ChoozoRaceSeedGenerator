91b907a705ff2f7a5b2819b0d7e51812eaddd87b
idlechild <>
2023-07-28 07:31:37 -0500

    Use pyz3r 6.0.8 and make discord bot more robust

--- DiscordGenerator.py
+++ DiscordGenerator.py
@@ -1,24 +1,24 @@
+import asyncio
 import datetime
 import discord
 from discord.ext import commands
 import os
 from os import system
 import traceback
+import SeedGenerator
 from SeedGenerator import ChoozoException
-from SeedGenerator import validate_choozo
-from SeedGenerator import generate_choozo
-from SeedGenerator import generate_smvaria
 
 
+exceptionrecursion = False
 roles = None
 intents = discord.Intents.all()
 discordbot = commands.Bot(command_prefix="!", intents=intents)
 
 
 async def discord_generate_choozo(ctx, race, split, area, boss, difficulty, escape, morph, start, title):
-    await validate_choozo(split, area, boss, difficulty, escape, morph, start)
+    await SeedGenerator.validate_choozo_params(split, area, boss, difficulty, escape, morph, start)
     await ctx.send("Generating seed...")
-    seed = await generate_choozo(race, split, area, boss, difficulty, escape, morph, start)
+    seed = await SeedGenerator.generate_choozo_seed(race, split, area, boss, difficulty, escape, morph, start)
 
     splitDescriptionDict = {
         "FullCountdown": "Full Countdown",
@@ -51,7 +51,7 @@ async def discord_generate_choozo(ctx, race, split, area, boss, difficulty, esca
 async def discord_generate_smvaria(ctx, settingsPreset, skillsPreset):
     await ctx.send("Generating seed...")
     settingsDict = {}
-    seed = await generate_smvaria(settingsPreset, skillsPreset, True, settingsDict)
+    seed = await SeedGenerator.generate_smvaria_seed(settingsPreset, skillsPreset, True, settingsDict)
     embedTitle = "Generated Super Metroid Race Seed"
     embedDescription = (
         f"**Settings: **{settingsPreset}\n"
@@ -72,11 +72,11 @@ async def discord_embed_seed(ctx, seed, embedTitle, embedDescription):
         value=seed.url,
         inline=False
     )
-    errorMsg = seed.data.get('errorMsg', '')
-    if errorMsg != '':
+    warnings = await SeedGenerator.parse_smvaria_warnings(seed)
+    if len(warnings) > 0:
         embed.add_field(
             name="Warnings",
-            value=errorMsg.replace("<br/>", "\n").rstrip(),
+            value='\n'.join(warnings),
             inline=False
         )
     await ctx.send(embed=embed)
@@ -88,133 +88,157 @@ async def discord_validate_channel(ctx):
             raise ChoozoException("Please go to the #practice channel to generate seeds")
 
 
-async def discord_get_arguments_provided_text(args):
-    return "%s %s provided" % (len(args), "argument" if 1 == len(args) else "arguments")
-
-
 async def discord_generate_choozo_parse_args(ctx, race, args):
     try:
         await discord_validate_channel(ctx)
-        if len(args) < 7:
-            argumentsProvided = await discord_get_arguments_provided_text(args)
-            raise ChoozoException("%s, 7 required (item split, area, boss, difficulty, escape, morph, start)" % argumentsProvided)
+        await SeedGenerator.validate_arguments(args, 7, -1, "7 required (item split, area, boss, difficulty, escape, morph, start)")
         await discord_generate_choozo(ctx, race, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7:])
     except ChoozoException as ce:
-        await ctx.send(ce.message)
+        await discord_send_message_to_channel(ctx, ce.message)
+    except Exception as e:
+        await discord_send_exception_to_sandbox(e)
 
 
 async def discord_generate_smvaria_parse_args(ctx, args):
     try:
         await discord_validate_channel(ctx)
-        if len(args) != 2:
-            argumentsProvided = await discord_get_arguments_provided_text(args)
-            raise ChoozoException("%s, 2 required (settings preset, skills preset)" % argumentsProvided)
+        await SeedGenerator.validate_arguments(args, 2, 2, "2 required (settings preset, skills preset)")
         await discord_generate_smvaria(ctx, args[0], args[1])
     except ChoozoException as ce:
-        await ctx.send(ce.message)
+        await discord_send_message_to_channel(ctx, ce.message)
+    except Exception as e:
+        await discord_send_exception_to_sandbox(e)
+
+
+async def discord_generate_sgl23smr_parse_args(ctx, args):
+    try:
+        await discord_validate_channel(ctx)
+        await SeedGenerator.validate_arguments(args, 0, 0, "0 expected")
+        await discord_generate_smvaria(ctx, "SGL23Online", "Season_Races")
+    except ChoozoException as ce:
+        await discord_send_message_to_channel(ctx, ce.message)
+    except Exception as e:
+        await discord_send_exception_to_sandbox(e)
 
 
 async def discord_help_parse_args(ctx, args):
     try:
         await discord_validate_channel(ctx)
-        if len(args) != 0:
-            argumentsProvided = await discord_get_arguments_provided_text(args)
-            raise ChoozoException("%s, 0 expected" % argumentsProvided)
+        await SeedGenerator.validate_arguments(args, 0, 0, "0 expected")
         await ctx.send("Choozo Race Seed Generator online")
     except ChoozoException as ce:
-        await ctx.send(ce.message)
-
-
-@discordbot.command()
-@commands.cooldown(1, 10, commands.BucketType.user)
-async def choozopractice(ctx, *args):
-    try:
-        await discord_generate_choozo_parse_args(ctx, False, args)
+        await discord_send_message_to_channel(ctx, ce.message)
     except Exception as e:
         await discord_send_exception_to_sandbox(e)
 
 
-@discordbot.command()
-@commands.cooldown(1, 10, commands.BucketType.user)
-async def choozorace(ctx, *args):
+async def discord_handle_role_react(payload, add):
     try:
-        await discord_generate_choozo_parse_args(ctx, True, args)
+        global roles
+        if 1021969894293647411 == payload.channel_id:
+            emojiDict = {
+                "🇦": "Async Race Admin",
+                "🇨": "Comms",
+                "🇵": "Practice",
+                "🇷": "Runner",
+                "🇹": "Tracking"
+            }
+            if payload.emoji.name in emojiDict:
+                reactRoleName = emojiDict[payload.emoji.name]
+                if roles is None:
+                    roles = await discordbot.guilds[0].fetch_roles()
+                for role in roles:
+                    if role.name == reactRoleName:
+                        if add:
+                            await payload.member.add_roles(role)
+                        else:
+                            for member in discordbot.get_all_members():
+                                if member.id == payload.user_id:
+                                    await member.remove_roles(role)
     except Exception as e:
         await discord_send_exception_to_sandbox(e)
 
 
-@discordbot.command()
-@commands.cooldown(1, 10, commands.BucketType.user)
-async def choozobot(ctx, *args):
-    try:
-        await discord_help_parse_args(ctx, args)
-    except Exception as e:
-        await discord_send_exception_to_sandbox(e)
-
-
-@discordbot.command()
-@commands.cooldown(1, 10, commands.BucketType.user)
-async def smvaria(ctx, *args):
-    try:
-        await discord_generate_smvaria_parse_args(ctx, args)
-    except Exception as e:
-        await discord_send_exception_to_sandbox(e)
-
-
-async def discord_on_role_react(payload, add):
-    global roles
-    if 1021969894293647411 == payload.channel_id:
-        emojiDict = {
-            "🇦": "Async Race Admin",
-            "🇨": "Comms",
-            "🇵": "Practice",
-            "🇷": "Runner",
-            "🇹": "Tracking"
-        }
-        if payload.emoji.name in emojiDict:
-            reactRoleName = emojiDict[payload.emoji.name]
-            if roles is None:
-                roles = await discordbot.guilds[0].fetch_roles()
-            for role in roles:
-                if role.name == reactRoleName:
-                    if add:
-                        await payload.member.add_roles(role)
-                    else:
-                        for member in discordbot.get_all_members():
-                            if member.id == payload.user_id:
-                                await member.remove_roles(role)
-
-
-@discordbot.event
-async def on_raw_reaction_add(payload):
-    try:
-        await discord_on_role_react(payload, True)
-    except Exception as e:
-        await discord_send_exception_to_sandbox(e)
-
-
-@discordbot.event
-async def on_raw_reaction_remove(payload):
-    try:
-        await discord_on_role_react(payload, False)
-    except Exception as e:
-        await discord_send_exception_to_sandbox(e)
+async def discord_send_message_to_channel(channel, message):
+    if not channel:
+        raise ChoozoException("Request to send message to empty channel")
+    if not message or 0 >= len(message):
+        raise ChoozoException("Request to send empty message")
+    while len(message) > 2000:
+        await channel.send(message[:2000])
+        message = message[2000:]
+    if len(message) > 0:
+        await channel.send(message)
 
 
 async def discord_send_message_to_sandbox(message):
     channel = discordbot.get_channel(1019847346344964126)
-    await channel.send(message)
+    await discord_send_message_to_channel(channel, message)
 
 
 async def discord_send_exception_to_sandbox(e):
-    message = ''.join(traceback.format_exception(None, e, e.__traceback__))
-    await discord_send_message_to_sandbox("Choozo Race Seed Generator Exception:\n%s" % message)
-    raise e
+    formatted_exception = ''.join(traceback.format_exception(None, e, e.__traceback__))
+    message = "Choozo Race Seed Generator Exception:\n%s" % formatted_exception
+    await discord_send_message_to_sandbox(message)
+
+
+async def discord_handle_exception(self, loop, context):
+    global exceptionrecursion
+    if not exceptionrecursion:
+        exceptionrecursion = True
+        message = "Choozo Race Seed Generator Exception:\n%s" % context
+        await discord_send_message_to_sandbox(message)
+        exceptionrecursion = False
+
+
+async def discord_handle_ready():
+    asyncio.get_event_loop().set_exception_handler(discord_handle_exception)
+    await discord_send_message_to_sandbox("Choozo Race Seed Generator started")
+
+
+@discordbot.command()
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def choozopractice(ctx, *args):
+    await discord_generate_choozo_parse_args(ctx, False, args)
+
+
+@discordbot.command()
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def choozorace(ctx, *args):
+    await discord_generate_choozo_parse_args(ctx, True, args)
+
+
+@discordbot.command()
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def choozobot(ctx, *args):
+    await discord_help_parse_args(ctx, args)
+
+
+@discordbot.command()
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def smvaria(ctx, *args):
+    await discord_generate_smvaria_parse_args(ctx, args)
+
+
+@discordbot.command()
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def sgl23smr(ctx, *args):
+    await discord_generate_sgl23smr_parse_args(ctx, args)
+
+
+@discordbot.event
+async def on_raw_reaction_add(payload):
+    await discord_handle_role_react(payload, True)
+
+
+@discordbot.event
+async def on_raw_reaction_remove(payload):
+    await discord_handle_role_react(payload, False)
 
 
 @discordbot.event
 async def on_ready():
-    await discord_send_message_to_sandbox("Choozo Race Seed Generator started")
+    await discord_handle_ready()
 
 
 @discordbot.event
--- SeedGenerator.py
+++ SeedGenerator.py
@@ -1,3 +1,4 @@
+import json
 import os
 from os import system
 from pyz3r.smvaria import SuperMetroidVaria
@@ -9,7 +10,68 @@ class ChoozoException(Exception):
         super().__init__("ChoozoException")
 
 
-async def validate_choozo(split, area, boss, difficulty, escape, morph, start):
+class VariaGenerator(SuperMetroidVaria):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.debugdefaults = False
+        self.debugsettings = False
+        self.debugskills = False
+
+
+    async def get_default_settings(self):
+        settings = await super().get_default_settings()
+        if self.debugdefaults:
+            debug_text = self.debug_settings_to_text(settings)
+            raise ChoozoException("VariaGenerator Defaults:\n%s" % debug_text)
+        return settings
+
+
+    async def get_settings(self):
+        settings = await super().get_settings()
+        if self.debugsettings:
+            debug_text = self.debug_settings_to_text(settings)
+            raise ChoozoException("VariaGenerator Settings:\n%s" % debug_text)
+        elif self.debugskills:
+            debug_text = self.debug_skills_to_text(settings["paramsFileTarget"], "")
+            raise ChoozoException("VariaGenerator Skills:\n%s" % debug_text)
+        return settings
+
+
+    def debug_settings_to_text(self, settings):
+        debug_text = ""
+        for key, value in sorted(settings.items()):
+            if key != "paramsFileTarget":
+                debug_text += "%s: %s\n" % (key, value)
+            elif not self.debugskills:
+                debug_text += "%s: %s\n" % (key, self.settings_preset)
+            else:
+                debug_text += "%s: {\n" % key
+                debug_text += self.debug_skills_to_text(value, "        ")
+                debug_text += "}\n"
+        return debug_text
+
+
+    def debug_skills_to_text(self, skills, indent):
+        debug_text = ""
+        debug_skills_data = json.loads(skills)
+        for key, value in sorted(debug_skills_data.items()):
+            if not isinstance(value, dict):
+                debug_text += "%s%s: %s\n" % (indent, key, value)
+            else:
+                debug_text += "%s%s: {\n" % (indent, key)
+                for k, v in sorted(value.items()):
+                    debug_text += "%s        %s: %s\n" % (indent, k, v)
+                debug_text += "%s}\n" % indent
+        return debug_text
+
+
+async def validate_arguments(args, minArgs, maxArgs, argsDescription):
+    if (len(args) < minArgs) or ((maxArgs >= 0) and (len(args) > maxArgs)):
+        argumentsProvided = "%s %s provided" % (len(args), "argument" if 1 == len(args) else "arguments")
+        raise ChoozoException("%s, %s" % (argumentsProvided, argsDescription))
+
+
+async def validate_choozo_params(split, area, boss, difficulty, escape, morph, start):
     if split not in ['FullCountdown', 'M/m', 'RandomSplit']:
         raise ChoozoException("Invalid item split setting.  Must be FullCountdown, M/m or RandomSplit.")
 
@@ -32,7 +94,7 @@ async def validate_choozo(split, area, boss, difficulty, escape, morph, start):
         raise ChoozoException("Invalid start setting.  Must be DeepStart, RandomStart, NotDeepStart, ShallowStart or VanillaStart.")
 
 
-async def generate_choozo(race, split, area, boss, difficulty, escape, morph, start):
+async def generate_choozo_seed(race, split, area, boss, difficulty, escape, morph, start):
     splitDict = {
         "FullCountdown": "FullWithHUD",
         "M/m": "Major",
@@ -119,11 +181,11 @@ async def generate_choozo(race, split, area, boss, difficulty, escape, morph, st
         "startLocationMultiSelect": startDict[start]
     }
 
-    return await generate_smvaria(settingsPreset, skillsPreset, race, settingsDict)
+    return await generate_smvaria_seed(settingsPreset, skillsPreset, race, settingsDict)
 
 
-async def generate_smvaria(settingsPreset, skillsPreset, race, settingsDict):
-    seed = await SuperMetroidVaria.create(
+async def generate_smvaria_seed(settingsPreset, skillsPreset, race, settingsDict):
+    seed = await VariaGenerator.create(
         settings_preset=settingsPreset,
         skills_preset=skillsPreset,
         race=race,
@@ -135,4 +197,12 @@ async def generate_smvaria(settingsPreset, skillsPreset, race, settingsDict):
         raise ChoozoException("Error: %s" % seed.data)
 
     return seed
+
+
+async def parse_smvaria_warnings(seed):
+    warnings = []
+    errorMsg = seed.data.get('errorMsg', '')
+    if len(errorMsg) > 0:
+        warnings = errorMsg.replace("<br/>", "\n").rstrip().split('\n')
+    return warnings
 
